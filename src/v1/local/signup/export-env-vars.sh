3a0ff6b0f7a619d65e6951d7bcc852a97a136e68
Thomas Step <tstep916@gmail.com>
2021-05-17 06:18:49 -0500

    Removing env vars

--- src/v1/local/signup/export-env-vars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-export USER_TABLE_NAME=
-export VERIFICATION_TTL=60
-export VERIFICATION_REDIRECT_URL=green-things.thomasstep.com
-export SITE_URL=green-things.thomasstep.com
-export EMAIL_FROM_ADDRESS=thomas@thomasstep.com
-export SENDGRID_API_KEY=
