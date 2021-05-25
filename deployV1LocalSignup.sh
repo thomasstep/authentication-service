e38e658abd1a878742653c1dd086dfdaa13348c0
Thomas Step <tstep916@gmail.com>
2021-05-24 19:17:42 -0500

    Updating deployment script to be more generic

--- deployV1LocalSignup.sh
+++ deployV1LocalSignup.sh
@@ -3,9 +3,11 @@
 # Change these to the correct values
 SOURCE_BUCKET_NAME=source-bucket-s3bucket-1e6txaiywjvno
 SOURCE_KEY=green-things-auth/v1/signup
+TEMPLATE_PATH=./infrastructure/api.yml
 STACK_NAME=green-things-auth
+STACK_SOURCE_VERSION_PARAMETER_NAME=V1SignupSourceVersion
 
 zip -r9 deployment-package ./src/v1/signup/index.js ./src/v1/signup/node_modules ./src/utils
 aws s3 cp deployment-package.zip s3://$SOURCE_BUCKET_NAME/$SOURCE_KEY
 S3_VERSION=$(aws s3api get-object --bucket $SOURCE_BUCKET_NAME --key $SOURCE_KEY getobjectoutfile | jq -r '.VersionId')
-aws cloudformation deploy --template-file ./infrastructure/api.yml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --parameter-overrides V1SignupSourceVersion=$S3_VERSION
+aws cloudformation deploy --template-file $TEMPLATE_PATH --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --parameter-overrides $STACK_SOURCE_VERSION_PARAMETER_NAME=$S3_VERSION
